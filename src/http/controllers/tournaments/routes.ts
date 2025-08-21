b3fa951c08d39920930e82566638684c8e50fd43
dmunhooz <deivid.munhoz42@gmail.com>
2025-08-21 15:19:23 -0300

    authorization based on role

--- src/http/controllers/tournaments/routes.ts
+++ src/http/controllers/tournaments/routes.ts
@@ -2,10 +2,11 @@ import { FastifyInstance } from 'fastify'
 import { verifyJWT } from '../../middlewares/verify-jwt'
 import { search } from './search'
 import { create } from './create'
+import { verifyUserRole } from '@/http/middlewares/verify-user-role'
 
 export async function tournamentsRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJWT)
 
   app.get('/tournaments/search', search)
-  app.post('/tournaments', create)
+  app.post('/tournaments', { onRequest: [verifyUserRole('ADMIN')] }, create)
 }
